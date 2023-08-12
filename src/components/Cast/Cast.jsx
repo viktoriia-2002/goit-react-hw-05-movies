20569b19458d17e1df66d43c3e078653a7b46bdc
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-08-12 16:14:19 -0400

    cast review

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,16 +1,48 @@
-import { useState, useCallback, useEffect } from 'react';
+// import { useState, useCallback, useEffect } from 'react';
+// import { useParams, Outlet } from 'react-router-dom';
+
+// import { getContent } from '../../utilities/api';
+
+// const Cast = () => {
+//   const [cast, setCast] = useState({});
+//   const params = useParams();
+//   const { movieId } = params;
+
+//   const fetchMovie = useCallback(async () => {
+//     const movieData = await getContent('movie', movieId);
+//     setCast(movieData.crew);
+//   }, [setCast, movieId]);
+
+//   useEffect(() => {
+//     fetchMovie();
+//   }, [fetchMovie]);
+
+//   console.log({ cast });
+
+//   return (
+//     <>
+//       <Outlet />
+//       Cast
+//     </>
+//   );
+// };
+
+// export default Cast;
+import React, { useState, useCallback, useEffect } from 'react';
 import { useParams, Outlet } from 'react-router-dom';
 
 import { getContent } from '../../utilities/api';
 
 const Cast = () => {
-  const [cast, setCast] = useState({});
+  const [cast, setCast] = useState([]);
   const params = useParams();
   const { movieId } = params;
 
+  const castUrl = 'http://image.tmdb.org/t/p/w200';
+
   const fetchMovie = useCallback(async () => {
     const movieData = await getContent('movie', movieId);
-    setCast(movieData.crew);
+    setCast(movieData.cast);
   }, [setCast, movieId]);
 
   useEffect(() => {
@@ -22,9 +54,19 @@ const Cast = () => {
   return (
     <>
       <Outlet />
-      Cast
+      <div>
+        {cast.map(({ profile_path, id, name, character }) => (
+          <div key={id}>
+            {profile_path && (
+              <img src={`${castUrl}${profile_path}`} alt="actor" />
+            )}
+            <h2>{name}</h2>
+            <p>Character: {character}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
-
 export default Cast;
